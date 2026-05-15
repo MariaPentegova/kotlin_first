643105df54d49bccd15da26e62a2e2e1a654aaa7
MariaPentegova <m.pentegova@yandex.ru>
2026-05-15 20:37:53 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "1.9.24"
 }
 
 group = "org.example"
@@ -10,11 +10,19 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    // Kotlin
+    implementation(kotlin("stdlib"))
+
+    // JUnit 5
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
 }
