a2c92fb5f9c3f20bed19b6fd42aed4503c78aad6
MariaPentegova <m.pentegova@yandex.ru>
2026-05-23 02:15:40 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,41 @@
+import gui.MainFrame
 import service.*
 import ui.ConsoleUI
+import javax.swing.JOptionPane
+import javax.swing.SwingUtilities
 
 fun main() {
-    val validator = BoardValidator()
-    val battleService = BattleService(validator)
-    val boardFactory = BoardFactory()
-    val gameManager = GameManager(validator, battleService, boardFactory)
-    val consoleUI = ConsoleUI(gameManager, boardFactory)
+    val choice = JOptionPane.showOptionDialog(
+        null,
+        "Выберите режим запуска:",
+        "Морской Бой",
+        JOptionPane.YES_NO_OPTION,
+        JOptionPane.QUESTION_MESSAGE,
+        null,
+        arrayOf("GUI", "Консоль"),
+        "GUI"
+    )
 
-    consoleUI.start()
+    when (choice) {
+        0 -> {
+            // GUI режим
+            SwingUtilities.invokeLater {
+                val frame = MainFrame()
+                frame.isVisible = true
+            }
+        }
+        1 -> {
+            // Консольный режим - создаём зависимости
+            val validator = BoardValidator()
+            val battleService = BattleService(validator)
+            val boardFactory = BoardFactory()
+            val registry = PlayerRegistry()  // для сохранения игроков
+            val gameManager = GameManager(validator, battleService, boardFactory, registry)
+            val consoleUI = ConsoleUI(gameManager, boardFactory)
+            consoleUI.start()
+        }
+        else -> {
+            println("Выход")
+        }
+    }
 }
